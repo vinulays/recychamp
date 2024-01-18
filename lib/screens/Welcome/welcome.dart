18f291a124a9e7a04789a9f763efde84ec5a6c46
Vinula Senarathne <vinulapc@gmail.com>
2024-01-18 18:42:20 +0530

    feat: welcome screen carrousel

--- lib/screens/Welcome/welcome.dart
+++ lib/screens/Welcome/welcome.dart
@@ -1,7 +1,17 @@
+import "package:carousel_slider/carousel_slider.dart";
+import "package:dots_indicator/dots_indicator.dart";
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
-class Welcome extends StatelessWidget {
+class Welcome extends StatefulWidget {
   const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +34,124 @@ class Welcome extends StatelessWidget {
                       bottomRight: Radius.circular(42.07),
                     ),
                   ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                          enableInfiniteScroll: false,
+                          height: MediaQuery.of(context).size.height * 0.77,
+                          viewportFraction: 1,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              _currentIndex = index;
+                            });
+                          }),
+                      items: [1, 2, 3, 4].map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                  children: [
+                                    Flexible(
+                                      child: SizedBox(
+                                        width: double.infinity,
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.55,
+                                        child: FittedBox(
+                                          fit: BoxFit.fill,
+                                          child: Image.asset(
+                                              "assets/images/welcome_girl.png"),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 40,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          width: 343,
+                                          child: Text.rich(
+                                            TextSpan(
+                                              children: [
+                                                TextSpan(
+                                                  text:
+                                                      'Little Start, Grows into ',
+                                                  style: GoogleFonts.almarai(
+                                                    color:
+                                                        const Color(0xFF1E1E1E),
+                                                    fontSize: 27.35,
+                                                    // fontFamily: 'Almarai',
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text: 'Something',
+                                                  style: GoogleFonts.almarai(
+                                                    color:
+                                                        const Color(0xFF75A488),
+                                                    fontSize: 27.35,
+                                                    // fontFamily: 'Almarai',
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text: ' Big',
+                                                  style: GoogleFonts.almarai(
+                                                    color:
+                                                        const Color(0xFF1E1E1E),
+                                                    fontSize: 27.35,
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        SizedBox(
+                                          width: 323,
+                                          height: 47,
+                                          child: Text(
+                                            'Where Fun Meets Conservation in Every Challenge!',
+                                            style: GoogleFonts.almarai(
+                                              color: const Color(0xFF747474),
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w300,
+                                              letterSpacing: 0.50,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ));
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 40, top: 10),
+                      child: DotsIndicator(
+                        dotsCount: 4,
+                        position: _currentIndex,
+                        decorator: DotsDecorator(
+                            size: const Size.square(9.0),
+                            activeSize: const Size(18.0, 9.0),
+                            activeShape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0)),
+                            activeColor: const Color(0xFF1E1E1E)),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),

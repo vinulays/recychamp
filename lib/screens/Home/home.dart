ed84d82192201550e42df221bf792d5438d35b61
Vinula Senarathne <vinulapc@gmail.com>
2024-01-22 09:49:51 +0530

    feat: latest challenges

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -3,6 +3,7 @@ import "package:flutter_svg/flutter_svg.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:recychamp/ui/bottom_app_bar.dart";
 import "package:recychamp/ui/home_three_row_button.dart";
+import "package:recychamp/ui/latest_challenges_card.dart";
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -116,159 +117,173 @@ class _HomeState extends State<Home> {
                     // * remaining content
                     Container(
                       margin: const EdgeInsets.only(top: 50),
-                      child: Column(
-                        children: [
-                          SvgPicture.asset(
-                            "assets/icons/gold-medal.svg",
-                            height: 32.08,
-                            width: 32.08,
-                          ),
-                          Column(
-                            children: [
-                              SizedBox(
-                                child: Text.rich(
-                                  TextSpan(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            SvgPicture.asset(
+                              "assets/icons/gold-medal.svg",
+                              height: 32.08,
+                              width: 32.08,
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(
+                                  child: Text.rich(
+                                    TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text:
+                                              "Your fresh and green comfortable ",
+                                          style: GoogleFonts.almarai(
+                                              color: const Color(0xFF1E1E1E),
+                                              fontSize: 27,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        TextSpan(
+                                          text: "place",
+                                          style: GoogleFonts.almarai(
+                                              color: const Color(0xFF75A488),
+                                              fontSize: 27,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                      ],
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: deviceData.size.width * 0.07),
+                                  // * Search bar row with filter button (yet to add functions)
+                                  child: Row(
                                     children: [
-                                      TextSpan(
-                                        text:
-                                            "Your fresh and green comfortable ",
-                                        style: GoogleFonts.almarai(
-                                            color: const Color(0xFF1E1E1E),
-                                            fontSize: 27,
-                                            fontWeight: FontWeight.w700),
+                                      Expanded(
+                                        child: TextField(
+                                          style: GoogleFonts.almarai(
+                                            fontSize: 17,
+                                          ),
+                                          decoration: InputDecoration(
+                                              contentPadding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 15),
+                                              prefixIconConstraints:
+                                                  const BoxConstraints(
+                                                      maxHeight: 26,
+                                                      minWidth: 26),
+                                              prefixIcon: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 13, right: 10),
+                                                child: SvgPicture.asset(
+                                                  "assets/icons/search.svg",
+                                                ),
+                                              ),
+                                              filled: true,
+                                              fillColor:
+                                                  const Color(0xffE6EEEA),
+                                              border: OutlineInputBorder(
+                                                borderSide: BorderSide.none,
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        12.62),
+                                              ),
+                                              hintStyle: GoogleFonts.almarai(
+                                                fontSize: 17,
+                                                color: const Color(0xff75A488),
+                                              ),
+                                              hintText: "Search Challenges"),
+                                        ),
                                       ),
-                                      TextSpan(
-                                        text: "place",
-                                        style: GoogleFonts.almarai(
-                                            color: const Color(0xFF75A488),
-                                            fontSize: 27,
-                                            fontWeight: FontWeight.w700),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      // * Inkwell to make button clickable
+                                      InkWell(
+                                        onTap: () {},
+                                        child: Container(
+                                          padding: const EdgeInsets.all(15),
+                                          decoration: const ShapeDecoration(
+                                            // color: Colors.black,
+                                            shape: RoundedRectangleBorder(
+                                              side: BorderSide(
+                                                color: Color(0xff75A488),
+                                              ),
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(12.62),
+                                              ),
+                                            ),
+                                          ),
+                                          child: SvgPicture.asset(
+                                              "assets/icons/filter.svg"),
+                                        ),
                                       ),
                                     ],
                                   ),
-                                  textAlign: TextAlign.center,
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Container(
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: deviceData.size.width * 0.07),
-                                // * Search bar row with filter button (yet to add functions)
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: TextField(
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                // * 3 button row
+                                Container(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: deviceData.size.width * 0.05),
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      HomeThreeRowButton(
+                                          iconURL:
+                                              "assets/icons/home-article.svg",
+                                          description: "Articles"),
+                                      HomeThreeRowButton(
+                                          iconURL: "assets/icons/home-cart.svg",
+                                          description: "Visit Shop"),
+                                      HomeThreeRowButton(
+                                          iconURL:
+                                              "assets/icons/home-calendar.svg",
+                                          description: "Calendar"),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      left: deviceData.size.width * 0.05),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "Latest Challenges",
                                         style: GoogleFonts.almarai(
-                                          fontSize: 17,
+                                          color: const Color(0xFF1E1E1E),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w700,
                                         ),
-                                        decoration: InputDecoration(
-                                            contentPadding:
-                                                const EdgeInsets.symmetric(
-                                                    vertical: 15),
-                                            prefixIconConstraints:
-                                                const BoxConstraints(
-                                                    maxHeight: 26,
-                                                    minWidth: 26),
-                                            prefixIcon: Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 13, right: 10),
-                                              child: SvgPicture.asset(
-                                                "assets/icons/search.svg",
-                                              ),
-                                            ),
-                                            filled: true,
-                                            fillColor: const Color(0xffE6EEEA),
-                                            border: OutlineInputBorder(
-                                              borderSide: BorderSide.none,
-                                              borderRadius:
-                                                  BorderRadius.circular(12.62),
-                                            ),
-                                            hintStyle: GoogleFonts.almarai(
-                                              fontSize: 17,
-                                              color: const Color(0xff75A488),
-                                            ),
-                                            hintText: "Search Challenges"),
                                       ),
-                                    ),
-                                    const SizedBox(
-                                      width: 20,
-                                    ),
-                                    // * Inkwell to make button clickable
-                                    InkWell(
-                                      onTap: () {},
-                                      child: Container(
-                                        padding: const EdgeInsets.all(15),
-                                        decoration: const ShapeDecoration(
-                                          // color: Colors.black,
-                                          shape: RoundedRectangleBorder(
-                                            side: BorderSide(
-                                              color: Color(0xff75A488),
-                                            ),
-                                            borderRadius: BorderRadius.all(
-                                              Radius.circular(12.62),
-                                            ),
-                                          ),
-                                        ),
-                                        child: SvgPicture.asset(
-                                            "assets/icons/filter.svg"),
-                                      ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              // * 3 button row
-                              Container(
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: deviceData.size.width * 0.05),
-                                child: const Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                // * Latest Challenges
+
+                                const Column(
                                   children: [
-                                    HomeThreeRowButton(
-                                        iconURL:
-                                            "assets/icons/home-article.svg",
-                                        description: "Articles"),
-                                    HomeThreeRowButton(
-                                        iconURL: "assets/icons/home-cart.svg",
-                                        description: "Visit Shop"),
-                                    HomeThreeRowButton(
-                                        iconURL:
-                                            "assets/icons/home-calendar.svg",
-                                        description: "Calendar"),
+                                    LatestChallengeCard(),
+                                    LatestChallengeCard(),
+                                    LatestChallengeCard(),
+                                    LatestChallengeCard(),
+                                    LatestChallengeCard(),
                                   ],
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 30,
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(
-                                    left: deviceData.size.width * 0.05),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "Latest Challenges",
-                                      style: GoogleFonts.almarai(
-                                        color: const Color(0xFF1E1E1E),
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              // * Latest Challenges
-                            ],
-                          )
-                        ],
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ],

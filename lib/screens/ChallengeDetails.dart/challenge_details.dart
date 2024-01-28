83b52b5c4e2b3c1d9bd70d68512cb48ab70e28be
Vinula Senarathne <vinulapc@gmail.com>
2024-01-28 20:16:45 +0530

    feat: challenge details header

--- lib/screens/ChallengeDetails.dart/challenge_details.dart
+++ lib/screens/ChallengeDetails.dart/challenge_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:recychamp/models/challenge.dart';
 
 class ChallengeDetails extends StatefulWidget {
@@ -17,16 +19,80 @@ class _ChallengeDetailsState extends State<ChallengeDetails> {
       body: Column(
         children: [
           // * challenge thumbnail
-          Container(
-            width: double.infinity,
-            height: 292,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image:
-                    AssetImage("assets/images/challenge_details_thumbnail_dummy.png"),
-                fit: BoxFit.fill,
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              ShaderMask(
+                blendMode: BlendMode.srcATop,
+                shaderCallback: (Rect bounds) {
+                  return LinearGradient(
+                    begin: const Alignment(0.01, 0),
+                    end: const Alignment(0.02, 0.80),
+                    colors: [Colors.black.withOpacity(0), Colors.black],
+                  ).createShader(bounds);
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 292,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          "assets/images/challenge_details_thumbnail_dummy.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
               ),
-            ),
+              // * back button to challenges list
+              Positioned(
+                top: 41.86,
+                left: 25.58,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: SvgPicture.asset("assets/icons/go_back.svg"),
+                ),
+              ),
+              // * category badge
+              Positioned(
+                top: 192,
+                left: 25.58,
+                child: Container(
+                  width: 101,
+                  height: 25,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.83),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Tree Planting",
+                        style: GoogleFonts.almarai(
+                            fontSize: 12, fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // * challenge title
+              Positioned(
+                top: 234,
+                left: 25.58,
+                child: Text(
+                  widget.challenge.title,
+                  style: GoogleFonts.almarai(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              )
+            ],
           )
         ],
       ),

9e2cd69716abc8450bf0e43d275ff173b400033d
Vinula Senarathne <vinulapc@gmail.com>
2024-02-06 19:25:40 +0530

    fix: security issues

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, TargetPlatform;
 
-import 'secrets.dart';
+import 'secrets.env';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {

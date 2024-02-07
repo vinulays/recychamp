ccfdb701cd872e31995380005e56a15be3e8fe27
Vinula Senarathne <vinulapc@gmail.com>
2024-02-07 20:15:53 +0530

    feat: challenge refresh indicator

--- lib/screens/Challenges/challenges.dart
+++ lib/screens/Challenges/challenges.dart
@@ -18,145 +18,152 @@ class Challenges extends StatefulWidget {
 
 class _ChallengesState extends State<Challenges> {
   List<Challenge> challenges = challengeList;
+
   @override
   Widget build(BuildContext context) {
     var deviceData = MediaQuery.of(context);
 
     return BlocBuilder<ChallengesBloc, ChallengesState>(
       builder: (context, state) {
+        final challengesBloc = BlocProvider.of<ChallengesBloc>(context);
         return Scaffold(
           resizeToAvoidBottomInset: false,
-          body: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(
-                    horizontal: deviceData.size.width * 0.07),
-                // * Headline row with settings button
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // * Headline left column
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // todo greeting must change according to the time of the day
-                        Text(
-                          "Challenges",
-                          style: GoogleFonts.almarai(
-                            // color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.w700,
+          body: RefreshIndicator(
+            onRefresh: () async {
+              challengesBloc.add(FetchChallengesEvent());
+            },
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                      horizontal: deviceData.size.width * 0.07),
+                  // * Headline row with settings button
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // * Headline left column
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // todo greeting must change according to the time of the day
+                          Text(
+                            "Challenges",
+                            style: GoogleFonts.almarai(
+                              // color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // todo Settings button (must link to settings page)
+                      SvgPicture.asset(
+                        "assets/icons/Settings.svg",
+                        height: 24,
+                        width: 24,
+                        colorFilter: const ColorFilter.mode(
+                            Colors.black, BlendMode.srcIn),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 60,
+                  margin: EdgeInsets.symmetric(
+                      horizontal: deviceData.size.width * 0.05),
+                  child: TextField(
+                    style: GoogleFonts.almarai(
+                      fontSize: 17,
+                    ),
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(14),
+                        prefixIconConstraints:
+                            const BoxConstraints(maxHeight: 26, minWidth: 26),
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.only(left: 13, right: 10),
+                          child: SvgPicture.asset(
+                            "assets/icons/search.svg",
                           ),
                         ),
-                      ],
-                    ),
-                    // todo Settings button (must link to settings page)
-                    SvgPicture.asset(
-                      "assets/icons/Settings.svg",
-                      height: 24,
-                      width: 24,
-                      colorFilter:
-                          const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Container(
-                height: 60,
-                margin: EdgeInsets.symmetric(
-                    horizontal: deviceData.size.width * 0.05),
-                child: TextField(
-                  style: GoogleFonts.almarai(
-                    fontSize: 17,
-                  ),
-                  decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.all(14),
-                      prefixIconConstraints:
-                          const BoxConstraints(maxHeight: 26, minWidth: 26),
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.only(left: 13, right: 10),
-                        child: SvgPicture.asset(
-                          "assets/icons/search.svg",
+                        suffixIconConstraints:
+                            const BoxConstraints(maxHeight: 26, minWidth: 26),
+                        // todo filter icon must open filter menu
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: InkWell(
+                            onTap: () {
+                              // * filter bottom drawer
+                              showModalBottomSheet(
+                                  shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.vertical(
+                                      top: Radius.circular(20),
+                                    ),
+                                  ),
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return const ChallengeFiltersBottomSheet();
+                                  });
+                            },
+                            child: SvgPicture.asset(
+                              "assets/icons/filter.svg",
+                            ),
+                          ),
                         ),
+                        filled: true,
+                        fillColor: const Color(0xffE6EEEA),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(12.62),
+                        ),
+                        hintStyle: GoogleFonts.almarai(
+                            fontSize: 17, color: const Color(0xff75A488)),
+                        hintText: "Search Challenges"),
+                  ),
+                ),
+
+                // * if challenges are loading, displays a loading circle
+                if (state is ChallengesLoading)
+                  const Expanded(
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        strokeCap: StrokeCap.round,
+                        strokeWidth: 5,
+                        color: Color(0xff75A488),
                       ),
-                      suffixIconConstraints:
-                          const BoxConstraints(maxHeight: 26, minWidth: 26),
-                      // todo filter icon must open filter menu
-                      suffixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 20),
-                        child: InkWell(
-                          onTap: () {
-                            // * filter bottom drawer
-                            showModalBottomSheet(
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.vertical(
-                                    top: Radius.circular(20),
+                    ),
+                  ),
+                if (state is ChallengesLoaded)
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: state.challenges.length,
+                        itemBuilder: (BuildContext context, index) {
+                          // * Gesture detector to navigate to details page when clicked on a challenge card
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ChallengeDetails(
+                                    challenge: state.challenges[index],
                                   ),
                                 ),
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return const ChallengeFiltersBottomSheet();
-                                });
-                          },
-                          child: SvgPicture.asset(
-                            "assets/icons/filter.svg",
-                          ),
-                        ),
-                      ),
-                      filled: true,
-                      fillColor: const Color(0xffE6EEEA),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(12.62),
-                      ),
-                      hintStyle: GoogleFonts.almarai(
-                          fontSize: 17, color: const Color(0xff75A488)),
-                      hintText: "Search Challenges"),
-                ),
-              ),
-
-              // * if challenges are loading, displays a loading circle
-              if (state is ChallengesLoading)
-                const Expanded(
-                  child: Center(
-                    child: CircularProgressIndicator(
-                      strokeCap: StrokeCap.round,
-                      strokeWidth: 5,
-                      color: Color(0xff75A488),
-                    ),
+                              );
+                            },
+                            child: ChallengeCard(
+                              challenge: state.challenges[index],
+                            ),
+                          );
+                        }),
                   ),
-                ),
-              if (state is ChallengesLoaded)
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: state.challenges.length,
-                      itemBuilder: (BuildContext context, index) {
-                        // * Gesture detector to navigate to details page when clicked on a challenge card
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ChallengeDetails(
-                                  challenge: state.challenges[index],
-                                ),
-                              ),
-                            );
-                          },
-                          child: ChallengeCard(
-                            challenge: state.challenges[index],
-                          ),
-                        );
-                      }),
-                ),
-            ],
+              ],
+            ),
           ),
         );
       },

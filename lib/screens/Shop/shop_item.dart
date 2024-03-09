96f9d9b88ea7507d52d75515ec0470eaf046ca08
MZ-MUZNI <93036314+MZ-MUZNI@users.noreply.github.com>
2024-03-09 16:09:46 +0530

    Create shop_item.dart

---
+++ lib/screens/Shop/shop_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopItem extends StatefulWidget {
+  const ShopItem({super.key});
+
+  @override
+  State<ShopItem> createState() => _ShopItemState();
+}
+
+class _ShopItemState extends State<ShopItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

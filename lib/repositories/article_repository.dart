1cd2b362ae7036b35bf2ee35fddbe0e65239c4f9
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2024-03-12 09:22:29 +0530

    article changed

---
+++ lib/repositories/article_repository.dart
@@ -0,0 +1,17 @@
+import 'package:recychamp/models/article_model.dart';
+import 'package:recychamp/services/article_service.dart';
+
+class ArticleRepo {
+  final ArticleService _articleServise;
+
+  ArticleRepo({required ArticleService articleServise})
+      : _articleServise = articleServise;
+
+  Future<List<Article>> getArticles() async {
+    return _articleServise.getArticles();
+  }
+
+  Future<void> addArticle(Map<String, dynamic> formData) async {
+    await _articleServise.addArticle(formData);
+  }
+}
--- lib/screens/EducationalResources/bloc/article_details_bloc.dart
+++ lib/screens/EducationalResources/bloc/article_details_bloc.dart
@@ -1,13 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
+import 'package:recychamp/models/article_model.dart';
+import 'package:recychamp/repositories/article_repository.dart';
 part 'article_details_event.dart';
 part 'article_details_state.dart';
 
-class ArticleDetailsBloc extends Bloc<ArticleDetailsEvent, ArticleDetailsState> {
-  ArticleDetailsBloc() : super(ArticleDetailsInitial()) {
-    on<ArticleDetailsEvent>((event, emit) {
-      // TODO: implement event handler
+class ArticleDetailsBloc
+    extends Bloc<ArticleDetailsEvent, ArticleDetailsState> {
+  final ArticleRepo _articleRepo;
+
+  ArticleDetailsBloc({required ArticleRepo repository})
+      : _articleRepo = repository,
+        super(ArticleDetailsInitial()) {
+    // * get challenges event from firebase
+    on<FetchArticleEvent>((event, emit) async {
+      emit(ArticleDetailsLoading());
+
+      try {
+        final articles = await _articleRepo.getArticles();
+
+        emit(ArticleDetailsLoaded(articles));
+      } catch (e) {
+        emit(ArticleDetailsError('Getting error'));
+      }
     });
   }
 }

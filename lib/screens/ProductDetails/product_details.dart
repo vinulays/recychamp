54fb59f9fa88fef44e3e77cbdabed8bb445820b8
MZ-MUZNI <93036314+MZ-MUZNI@users.noreply.github.com>
2024-03-20 16:51:47 +0530

    product update

--- lib/screens/ProductDetails/product_details.dart
+++ lib/screens/ProductDetails/product_details.dart
@@ -67,17 +67,68 @@ class _ProductDetailsState extends State<ProductDetails> {
               ),
             ),
           ),
-          SizedBox(
+          Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: deviceData.size.width * 0.05),
               height: 300,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const SizedBox(
-                    height: 20,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        widget.product.name,
+                        style: GoogleFonts.poppins(
+                            fontSize: 25, fontWeight: FontWeight.w700),
+                      ),
+                      Text(
+                        "LKR ${widget.product.price.toStringAsFixed(2)}",
+                        style: GoogleFonts.poppins(fontSize: 25),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        widget.product.description,
+                        style: GoogleFonts.poppins(fontSize: 16),
+                      ),
+                    ],
                   ),
-                  Text(
-                    widget.product.name,
-                    style: GoogleFonts.poppins(
-                        fontSize: 25, fontWeight: FontWeight.w700),
+                  Container(
+                    margin: EdgeInsets.only(
+                        right: deviceData.size.width * 0.02,
+                        left: deviceData.size.width * 0.02,
+                        bottom: 10),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16.8),
+                            ),
+                          ),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.symmetric(vertical: 17.88)),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                        ),
+                        child: Text(
+                          "Add to cart",
+                          style: GoogleFonts.poppins(
+                              fontSize: 19,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ))

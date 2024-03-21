abeea933c03c25c8b1a22cbd5c11b9a278c2a545
MZ-MUZNI <93036314+MZ-MUZNI@users.noreply.github.com>
2024-03-21 21:29:15 +0530

    cart updates

--- lib/screens/Cart/bloc/cart_state.dart
+++ lib/screens/Cart/bloc/cart_state.dart
@@ -1,10 +1,20 @@
 part of 'cart_bloc.dart';
 
 sealed class CartState extends Equatable {
-  const CartState();
-  
   @override
   List<Object> get props => [];
 }
 
 final class CartInitial extends CartState {}
+
+class CartLoadedState extends CartState {
+  final Cart cart;
+
+  CartLoadedState(this.cart);
+}
+
+class CartErrorState extends CartState {
+  final String errorMessage;
+
+  CartErrorState(this.errorMessage);
+}
